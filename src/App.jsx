1597e791274381e33a2f4edaee1c77118218d420
icetea1223 <hongkimchhay286@gmail.com>
2025-12-16 18:31:48 +0700

    t commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./conponent/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
-import Skill from "./pages/Skill/Skill";
-import Project from "./pages/Project/Project";
+import Navbar from "./conponent/Navbar/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
+import About from "./pages/About/About.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import Skill from "./pages/Skill/Skill.jsx";
+import Project from "./pages/Project/Project.jsx";
 
 function App() {
   return (
