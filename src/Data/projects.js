4f7a36e9e137604f7ba8e4dce15e221a93ed1f86
icetea1223 <hongkimchhay286@gmail.com>
2026-01-08 14:02:12 +0700

    first commit

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -7,7 +7,8 @@ const projects = [
     tech: ["React", "Tailwind"],
     status: "Live",
     year: "2024",
-    image: "../../src/assets/images/web1.png",
+    image:
+      "https://scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/612152136_1569394897641910_1245838516445074061_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEJS-YabRc_HoFuddAb3bCWkB1JjwTsgKSQHUmPBOyApPRAuWa2y4sipZD3CsXT3HT_pfevvDS1MI2BVFk_QYU9&_nc_ohc=4ebMrs9WAoQQ7kNvwFkUkUa&_nc_oc=AdksRbu4hyIJ_LaBGw2xcCgNqBqzUBLEkRZmW_xCjgKwMenYwDJHavJ3ia-krSAta7s&_nc_zt=23&_nc_ht=scontent.fpnh5-2.fna&_nc_gid=7yL-bS3YKc8Sqylo7oW7IA&oh=00_AfrLmVKU_CNnZsCf_uJpuu6yAUtxlftNCy65eA3dhAe76g&oe=69653592",
     liveUrl: "https://auto-car-react-js-pwqt.vercel.app/",
     githubUrl: "https://github.com/IceTea1223/Auto-Car-ReactJs-.git",
     featured: true,
@@ -19,10 +20,10 @@ const projects = [
     title: "Movie",
     description: "If you want date with specail person can view my website.",
     category: ["web", "Frontend"],
-    tech: ["HTML", "CSS" , "Bootstrap"],
+    tech: ["HTML", "CSS", "Bootstrap"],
     status: "Live",
     year: "2024",
-    image: "../../src/assets/images/web2.png",
+    image: "https://scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/612417093_1569394917641908_2894255227961323203_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGjp8Elm-EhmkRiHfRiMkmsKskkF79xX2UqySQXv3FfZa0NhEeMWDs1vsY4YzhnKisoCdM4OdaWhlmWcSi7JHHn&_nc_ohc=QGMj20HTr-IQ7kNvwF3jpXj&_nc_oc=Adl3dvC-r1P83Gk0nTorHL3UFxlE5_8pz0A4gGrlwPKsn5IKxVGUKE2wzQTdFzL7CKc&_nc_zt=23&_nc_ht=scontent.fpnh5-2.fna&_nc_gid=Ohb2Nn2LzvpLSGykMJufuw&oh=00_AfqZwaWeuYshg0Jbomcxc0S_oDjLDrKRQhGsTwiS2GXgjA&oe=69653B24",
     liveUrl: "https://movie-project-168.vercel.app/",
     githubUrl: "https://github.com/IceTea1223/Movie_project.git",
     featured: true,
@@ -34,10 +35,10 @@ const projects = [
     title: "Food",
     description: "If you want order food can view my website.",
     category: ["web", "Frontend"],
-    tech: ["HTML", "CSS" , "Bootstrap"],
+    tech: ["HTML", "CSS", "Bootstrap"],
     status: "Live",
     year: "2024",
-    image: "../../src/assets/images/web3.png",
+    image: "https://scontent.fpnh5-4.fna.fbcdn.net/v/t39.30808-6/612897038_1569394894308577_2518065511344673934_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGsdXJaQrRTc1gB7eEjQG6-BWkPIMOStXwFaQ8gw5K1fNGzJ2l15l6KP1dyj8xXzRjwe9B0JiIxfMiGIlo5w87x&_nc_ohc=1Ze-_n_q-o8Q7kNvwERUpKK&_nc_oc=AdmwxxDwvFPLAJsZw6Q8NmMDL8D1aa9lJBwImemSSRwzmo2BHA6Pfl5v-EyM9nufQNQ&_nc_zt=23&_nc_ht=scontent.fpnh5-4.fna&_nc_gid=0K_13p0FsBL5nJofmay28A&oh=00_AfofMs2uJjdqDxtdD11xVJ3poTdtW5QRBW4FMwcplXhJQg&oe=69651E8F",
     liveUrl: "https://food-project168.vercel.app/",
     githubUrl: "https://github.com/IceTea1223/Food-project.git",
     featured: true,
@@ -46,5 +47,4 @@ const projects = [
   },
 ];
 
-
- export default projects;
+export default projects;
